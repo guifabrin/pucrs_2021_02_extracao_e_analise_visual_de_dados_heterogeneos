24747e8df64f6fc8372db02dd786eaeb66c36b86
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2021-08-18 20:09:39 -0300

    Add more data

--- twitter.py
+++ twitter.py
@@ -1,41 +1,47 @@
 import tweepy
 import json
 import os
-import threading
 
 auth = tweepy.OAuthHandler('46pvnSwIVylfWepbPsP4433wL', 'xWDPHaUkk0ub93qj1DaYgJcO8QtkPUhNFIE7uBAvzbSVLLpLzR')
-
 auth.set_access_token('1952916806-9WbU9ROPLd4aVPprQZqWJhaW4RSXrBw4oK8A4Ow',
                       'gW5iuYPtrTxVhPmQxBemsKz6jCAOqbYx1fT0ewKHFyAkG')
-
 api = tweepy.API(auth)
+page = 0
+done = []
+user = 'elonmusk'
+total_tweets = 16000
+directory = './cache/twitter/' + user + '/'
+
+if os.path.isfile(directory + '/done.json'):
+    file = open(directory + '/done.json', )
+    done = json.load(file)
 
 
-def obter_tweets(user, limit=10, page=1):
-    return api.user_timeline(screen_name=user, count=limit, tweet_mode='extended', page=page)
-
-
-def th(page):
-    tweets = obter_tweets(user=user, limit=200, page=page)
-    directory = './cache/twitter/' + user + '/'
+def fetch_tweets(number_page):
+    limit = 200
+    tweets = api.user_timeline(screen_name=user, count=limit, tweet_mode='extended', page=page)
     if not os.path.exists(directory):
         os.makedirs(directory)
-    if len(tweets) != 200:
-        return th(page)
     for tweet in tweets:
         filename = directory + tweet.created_at.strftime("%Y%m%d%H%M%S") + '.json'
         if os.path.isfile(filename):
+            print('Tweet', tweet.created_at, 'already recovered')
             continue
         with open(filename, 'w') as outfile:
             json.dump(tweet._json, outfile)
-    print('finalizado', page)
+    if len(tweets) != limit:
+        print('Limit does not matching on page', page, 're trying')
+        return fetch_tweets(number_page)
+    print('Ended', number_page)
+    done.append(number_page)
+    with open(directory + '/done.json', 'w') as outfile:
+        json.dump(done, outfile)
 
 
-user = 'elonmusk'
-page = 1
-total = 16000
-print(total/200)
-while page < total/200:
-    x = threading.Thread(target=th, args=(page,))
-    x.start()
-    page += page + 1
+while page < total_tweets / 200:
+    page += 1
+    if page in done:
+        print('Page', page, 'already fetched')
+        continue
+    print('Starting', page)
+    fetch_tweets(page)
